43beacfca615d9848b46408fc29dd0271257572b
Choi Jiwon <winning1993@naver.com>
2020-05-19 22:58:07 +0900

    오답 정답 체크

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const inputArea = document.getElementById("inputArea");
-
+const inputArea = document.getElementById("inputArea"),
+wordArea = document.getElementById("wordArea"),
+manPart = document.getElementsByClassName("man-part");
 
 let keyword = "";
+let j = 0;
 
 function ramdomWord(){
     const randomNum = Math.floor(Math.random()*10);
@@ -16,17 +18,35 @@ function ramdomWord(){
         span.className = "keyword_area"
         span.innerHTML = "___";
         inputArea.appendChild(span);
+        const span2 = document.createElement("span");
+        span2.className = "show-words";
+        span2.id = `showWord${i}`;
+        span2.innerHTML = keyword[i];
+        wordArea.appendChild(span2);
     }
 }
 
 function handleTyping(e){
     console.log(e.key)
+    let match = false;
+    for(let i=0; i<keyword.length; i++){
+        if(e.key === keyword[i]){
+            const target = document.getElementById(`showWord${i}`);
+            target.style.opacity = "1";
+            match = true;
+        }
+        
+    }
+    if(match === false){
+            manPart[j].style.opacity = 1;
+        j++;
+    }
 }
 
 
 function init(){
+    ramdomWord();
     window.addEventListener("keydown", handleTyping);
-    ramdomWord()
 }
 
 init();
