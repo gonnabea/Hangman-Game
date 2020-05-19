b38f578805634eaa290e82cd52de7fcaf9a08e25
Choi Jiwon <winning1993@naver.com>
2020-05-20 04:37:33 +0900

    틀렸을 경우 모달창, 다시시도 정상 구현

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputArea = document.getElementById("inputArea"),
-wordArea = document.getElementById("wordArea"),
-manPart = document.getElementsByClassName("man-part");
+manPart = document.getElementsByClassName("man-part"),
+modalContainer = document.getElementById("modalContainer"),
+replayBtn = document.getElementById("replayBtn");
 
 let keyword = "";
 let j = 0;
@@ -12,35 +13,60 @@ function ramdomWord(){
     "expedition", "edit", "optical"];
     keyword = words[randomNum];
     console.log(keyword);
+    const div = document.createElement("div");
+    div.id = "wordArea";
+    div.className = "word-area";
+    inputArea.appendChild(div);
     
     for(let i = 0; i < keyword.length ; i++){
         const span = document.createElement("span");
-        span.className = "keyword_area"
+        span.className = "keyword_area";
         span.innerHTML = "___";
         inputArea.appendChild(span);
         const span2 = document.createElement("span");
         span2.className = "show-words";
         span2.id = `showWord${i}`;
         span2.innerHTML = keyword[i];
-        wordArea.appendChild(span2);
+        div.appendChild(span2);
     }
-}
+} // 정답 생성, 정답 글자 수 체크 후 밑줄 생성.
 
 function handleTyping(e){
-    console.log(e.key)
+    console.log(e.key);
     let match = false;
+    
     for(let i=0; i<keyword.length; i++){
         if(e.key === keyword[i]){
             const target = document.getElementById(`showWord${i}`);
-            target.style.opacity = "1";
+            target.style.opacity = 1;
             match = true;
         }
         
-    }
-    if(match === false){
+    } // 정답인 지 체크
+    if(match === false && j < 6){
             manPart[j].style.opacity = 1;
         j++;
-    }
+    } // 실패했을 경우, 아직 기회가 남아있음
+    if(j === 6){
+        window.removeEventListener("keydown", handleTyping);
+        modalContainer.style.display = "flex";
+        replayBtn.addEventListener("click", restart);
+    } // 패배
+}
+
+function restart(){
+    replayBtn.removeEventListener("click", restart);
+    window.addEventListener("keydown", handleTyping);
+    modalContainer.style.display = "none";
+    const keywordArea = document.getElementsByClassName("keyword_area");
+
+    for(let i=0; i<6; i++){
+        manPart[i].style.opacity = 0;
+    } // 행맨 초기화
+    console.log(keywordArea)
+    inputArea.innerHTML = "";
+    j=0;
+    ramdomWord();
 }
 
 
